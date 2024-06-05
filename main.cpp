aaf5a3fe5cc5c3798541f4c624b24507f4a79036
YackswebCode <yahayaibraheem808@gmail.com>
2024-06-05 21:01:38 +0100

    Create main.cpp
    interface frame

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#### `main.cpp`
+
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return app.exec();
+}
+
+
+
